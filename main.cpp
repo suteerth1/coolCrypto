1fc1ed9222c3853ee886f84415bcbe55f18859b0
suteerth <svishnu@umd.edu>
2016-03-19 00:09:10 -0400

    Minor fixes in C.L.I.

--- main.cpp
+++ main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include "CCWindow.h"
-#include <gtkmm/main.h>
+
 
 
 using namespace std;
 
 int main(int argc,char** argv) {
     Gtk::Main kit(argc,argv);
-
     CoolCryptoWindow window;
-
     Gtk::Main::run(window);
-
-    cout << "Hello, World!" << endl;
     return 0;
 }
